cf72bd0fec7bbddd70c8f2a85dc8e04fb18ef778
ToTuyen04 <tuyentmse183888@fpt.edu.vn>
2025-12-30 18:14:41 +0700

    polly policy in ocelot

--- eCommerceSolution.OrdersService/OrdersMicroservice.API/obj/Debug/net8.0/OrdersMicroservice.API.AssemblyInfo.cs
+++ eCommerceSolution.OrdersService/OrdersMicroservice.API/obj/Debug/net8.0/OrdersMicroservice.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrdersMicroservice.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba46414df95e373e2c2933dec7a006ab62819a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97439d35a095c2087f592c149bf77d856d6b2a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrdersMicroservice.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrdersMicroservice.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
